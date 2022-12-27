f2f73d9b8a7f4a44cdcfb1251195988d0fecf355
taeyun1215 <taeyun1215@naver.com>
2022-12-27 22:50:40 +0900

    docker test plz

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,4 +19,4 @@ echo "5"
 docker run -d -p 8080:8080 --name mkc taeyun1215/mkc
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
-docker rmi -f $(docker images -f "dangling=true" -q) || true
+#docker rmi -f $(docker images -f "dangling=true" -q) || true
