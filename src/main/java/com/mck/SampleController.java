e2981633305c229b6f178929e8ef6e5dda36321d
taeyun1215 <taeyun1215@naver.com>
2022-12-27 23:19:25 +0900

    docker test plz

---
+++ src/main/java/com/mck/SampleController.java
@@ -0,0 +1,13 @@
+package com.mck;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/sample")
+    public String sample() {
+        return "plzzzzzz!!!";
+    }
+}
