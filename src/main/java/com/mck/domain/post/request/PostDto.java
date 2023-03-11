4058693b1135db2ed3934618d2373b2206c2931d
taeyun1215 <taeyun1215@naver.com>
2023-03-11 16:44:59 +0900

    config : restful api

--- src/main/java/com/mck/domain/post/request/PostDto.java
+++ src/main/java/com/mck/domain/post/request/PostDto.java
@@ -5,6 +5,7 @@ import com.mck.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
