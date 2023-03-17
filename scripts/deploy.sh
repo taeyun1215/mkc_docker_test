f8f0ac2ab8bfa00edad0b97a32fa30c1a9a277bc
Lee Tae Yun <65766105+taeyun1215@users.noreply.github.com>
2023-03-17 16:20:04 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,9 @@ sudo systemctl start docker
 
 # 가동중인 mkc 도커 중단 및 삭제
 echo "2"
-docker ps -a -q --filter "name=mkc" | grep -q . && docker stop mkc && docker rm mkc | true
+docker rm -f $(docker ps -qa)
+
+# docker ps -a -q --filter "name=mkc" | grep -q . && docker stop mkc && docker rm mkc | true
 
 # 기존 이미지 삭제
 echo "3"
@@ -16,7 +18,8 @@ docker pull taeyun1215/mkc
 
 # 도커 run
 echo "5"
-docker run -d -p 8080:8080 --name mkc taeyun1215/mkc
+docker-compose up -d
+# docker run -d -p 8080:8080 --name mkc taeyun1215/mkc
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 #docker rmi -f $(docker images -f "dangling=true" -q) || true
