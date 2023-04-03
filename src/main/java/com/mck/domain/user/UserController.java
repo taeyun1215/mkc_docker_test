9ece15622009c21a6d7caad51bc8327def6a2c09
guhyeon <glpat-BzwPeDgmP6sCysK-Vhtf>
2023-04-03 15:00:43 +0900

    회원 탈퇴 수정

--- src/main/java/com/mck/domain/user/UserController.java
+++ src/main/java/com/mck/domain/user/UserController.java
@@ -189,7 +189,7 @@ public class UserController {
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             try {
                 String token = authorizationHeader.substring("Bearer ".length());
-                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                Algorithm algorithm = Algorithm.HMAC256(secretKey.getBytes());
                 JWTVerifier verifier = JWT.require(algorithm).build();
                 DecodedJWT decodedJWT = verifier.verify(token);
                 String username = decodedJWT.getSubject();
